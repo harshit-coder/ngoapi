1bb92aa9acd4caf937c547d32a9bc7cf719bffdf
Harshit Kalucha <kaluchaharshit22@gmail.com>
2021-08-07 22:54:23 +0530

    make it better

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import concurrent.futures
 import os
-
 from bs4 import BeautifulSoup as soup
 from flask import Flask, render_template, request
 from flask import send_file
@@ -10,6 +10,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 app = Flask(__name__)
+
+
+class NGO:
+    def collect(self, url, i, options, wb, sh1, filename):
+        print(str(i))
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+        driver.get(url)
+        driver.implicitly_wait(10)
+        all = driver.find_element_by_xpath("/html/body/div[9]/div[1]/div[3]/div/div/div[2]/table/tbody/tr[" + str(i) + "]/td[2]/a")
+        all.click()
+        overlay = WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "div.blockUI.blockOverlay")))
+        if overlay:
+            close = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ngo_info_modal']/div[2]/div/div[1]/button/span")))
+            close.click()
+        ngo = driver.find_element_by_id('ngo_info_modal')
+        result = ngo.get_attribute('innerHTML')
+        sp = soup(result, 'html.parser')
+        name = sp.find(id="ngo_name_title")
+        add = sp.find(id="address")
+        date = sp.find(id="ngo_reg_date")
+        mobile = sp.find(id="mobile_n")
+        email = sp.find(id="email_n")
+        # print("row", str(i + 1))
+        # print("name:", name.text)
+        # print("add:", add.text)
+        # print("date", date.text)
+        # print("mobile", mobile.text)
+        # print("email", email.text)
+        line = str(i + 1), name.text, add.text, date.text, mobile.text, email.text
+        print(line)
+        sh1.cell(row=i + 1, column=1, value=name.text)
+        sh1.cell(row=i + 1, column=2, value=date.text)
+        sh1.cell(row=i + 1, column=3, value=add.text)
+        sh1.cell(row=i + 1, column=4, value=mobile.text)
+        sh1.cell(row=i + 1, column=5, value=email.text)
+        wb.save(filename + ".xls")
+        driver.close()
+        return line
 
 
 @app.route('/')
@@ -47,44 +85,23 @@ def scrape():
     filename = request.form.get("page")
     wb.save(filename + ".xls")
     if request.method == "POST":
-        for i in range(int(start), int(rows)+1):
-            print(str(i))
-            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
-            driver.get(url)
-            driver.implicitly_wait(10)
-            all = driver.find_element_by_xpath("/html/body/div[9]/div[1]/div[3]/div/div/div[2]/table/tbody/tr[" + str(i) + "]/td[2]/a")
-            all.click()
-            overlay = WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "div.blockUI.blockOverlay")))
-            if overlay:
-                close = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ngo_info_modal']/div[2]/div/div[1]/button/span")))
-                close.click()
-            ngo = driver.find_element_by_id('ngo_info_modal')
-            result = ngo.get_attribute('innerHTML')
-            sp = soup(result, 'html.parser')
-            name = sp.find(id="ngo_name_title")
-            add = sp.find(id="address")
-            date = sp.find(id="ngo_reg_date")
-            mobile = sp.find(id="mobile_n")
-            email = sp.find(id="email_n")
-            # print("row", str(i + 1))
-            # print("name:", name.text)
-            # print("add:", add.text)
-            # print("date", date.text)
-            # print("mobile", mobile.text)
-            # print("email", email.text)
-            line = str(i + 1), name.text, add.text, date.text, mobile.text, email.text
-            print(line)
-            sh1.cell(row=i + 1, column=1, value=name.text)
-            sh1.cell(row=i + 1, column=2, value=date.text)
-            sh1.cell(row=i + 1, column=3, value=add.text)
-            sh1.cell(row=i + 1, column=4, value=mobile.text)
-            sh1.cell(row=i + 1, column=5, value=email.text)
-            wb.save(filename + ".xls")
-            driver.close()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=int(rows)) as executor:
+            future_to_url = {executor.submit(ngo.collect, url, i, options, wb, sh1, filename): i for i in range(int(start), int(rows) + 1)}
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                print("url", url)
+                try:
+                    data = future.result()
+                    print("data", data)
+                except Exception as exc:
+                    print('%r generated an exception: %s' % (url, exc))
+                else:
+                    print('%r page is %d bytes' % (url, len(data)))
 
     response = send_file(filename + ".xls")
     return response
 
 
+ngo = NGO()
 if __name__ == "__main__":
     app.run(debug=True)
